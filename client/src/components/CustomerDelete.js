d1e6517903e59ed229fe1e7b59dba121c3d6c186
memories109 <memories109@naver.com>
2021-10-17 11:30:39 +0900

    Add Material UI & ADD and DELETE

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,35 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { ThemeProvider, unstable_createMuiStrictModeTheme } from '@material-ui/core';
+
+const theme = unstable_createMuiStrictModeTheme();
 
 class CustomerDelete extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open :false
+        }
+      
+    }
+
+    handleClickOpen = () => {
+        this.setState ({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
 
     deleteCustomer(id) {
         const url = '/api/customers/' + id;
@@ -9,11 +38,26 @@ class CustomerDelete extends React.Component{
         });
         this.props.stateRefresh();
     }
-    
+
     render() {
         return (
-            <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</button>
-
+            <ThemeProvider theme={theme}>
+            <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle onClose={this.handleClose}>
+                    삭제 경고
+                </DialogTitle>
+                <DialogContent >
+                    <Typography gutterBottom>
+                        선택한 정보가 삭제됩니다.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>삭제</Button>
+                    <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                </DialogActions>
+            </Dialog>
+            </ThemeProvider>
         )
     }
 }
